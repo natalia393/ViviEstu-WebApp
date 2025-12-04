9d7dac9697d3fd8448d01f6117bfb8dd85e17f49
Natalia <u20221c602@upc.edu.pe>
2025-12-04 01:28:09 -0500

    cambio4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    production: false,
-    base: 'http://localhost:8080'
+    //production: false,
+    //base: 'http://localhost:8080'
+
+    production: true, // Cambiamos a true porque ya está en la nube
+    base: 'https://viviestuv2.onrender.com' // Solo el dominio, sin la parte de swagger
 }
